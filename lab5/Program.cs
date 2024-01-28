9ab46d97424aa49dc96a2a5b14269d916493624b
danil <dddd@gmail.com>
2024-01-28 12:53:23 +0300

    Добавьте файлы проекта.

---
+++ lab5/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("введите значение x = ");
+double x = double.Parse(Console.ReadLine());
+double s = 0;
+int n = 0;
+double a = 10 / x;
+const double e = 0.001;
+while(a>=e)
+{
+    s=s+a; 
+    n++;
+    a = a / n;
+}
+Console.WriteLine($"s ={s} n={n}");
